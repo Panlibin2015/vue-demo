3375f4851a27ce8e36bbcb971bfedd0f4d3e5ba2
Panlibin2015 <18267568873@163.com>
2023-08-03 16:15:07 +0800

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,17 @@ const msg = ref('文本插值');
 const isButtonDisabled = ref(true);
 const dynamicId = ref('text');
 const rawHtml = ref('<span style="color: red">This should be red.</span>');
-
 const objectOfAttrs = {
   id: 'container',
   class: 'wrapper',
 };
+
+const number = ref(0);
+const ok = ref(true);
+const message = ref('hello');
+const id = ref(0);
+
+const seen = ref(true);
 </script>
 
 <template>
@@ -27,7 +33,14 @@ const objectOfAttrs = {
     <h3>动态绑定多个值</h3>
     <div v-bind="objectOfAttrs"></div>
     <h1>使用JavaScript表达式</h1>
-    "{{ var a = 1}}"
+    <span>{{ number + 1 }}</span>
+    <br />
+    {{ ok ? 'YES' : 'NO' }}
+    <br />{{ message.split('').reverse().join('') }}
+    <div :id="`list-${id}`"></div>
+    <h1>指令 Directives</h1>
+    <a v-if="seen">Now you see me</a>
+    <img src="https://cn.vuejs.org/assets/directive.69c37117.png" />
   </div>
 </template>
 
