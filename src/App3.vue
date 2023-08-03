fe837dff7d1cae8f4b2c1ae01015a2ec06da1526
Panlibin2015 <18267568873@163.com>
2023-08-03 16:31:49 +0800

    1 file modified, 1 added

---
+++ src/App3.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { reactive } from 'vue';
+// `setup` 是一个特殊的钩子，专门用于组合式 API。
+const state = reactive({ count: 0 });
+function increment() {
+  // 在 JavaScript 中需要 .value
+  state.count++;
+}
+</script>
+
+<template>
+  <div>{{ state.count }}</div>
+  <button @click="state.count++">按钮</button>
+  <button @click="increment">自增</button>
+</template>
+
+<style></style>
