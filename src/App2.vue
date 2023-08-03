a5d9b9c9159a97df991c871db02909ac07c65e9e
Panlibin2015 <18267568873@163.com>
2023-08-03 16:24:10 +0800

    1 file modified, 1 added

---
+++ src/App2.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+// `setup` 是一个特殊的钩子，专门用于组合式 API。
+const count = ref(0);
+function increment() {
+  // 在 JavaScript 中需要 .value
+  count.value++;
+}
+</script>
+
+<template>
+  <div>{{ count }}</div>
+  <button @click="count++">按钮</button>
+  <button @click="increment">自增</button>
+</template>
+
+<style></style>
