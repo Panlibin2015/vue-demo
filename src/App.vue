c7ecc616e95659bf63ffdffa3ef94b6f26c7e07b
Panlibin2015 <18267568873@163.com>
2023-08-03 17:59:56 +0800

    1 file modified, 2 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,65 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, reactive } from 'vue';
+const isActive = ref(true);
 
-const firstName = ref('John');
-const lastName = ref('Doe');
+const classObject = computed(() => ({
+  active: isActive.value,
+}));
 
-const fullName = computed({
-  // getter
-  get() {
-    return firstName.value + ' ' + lastName.value;
-  },
-  // setter
-  set(newValue) {
-    // 注意：我们这里使用的是解构赋值语法
-    [firstName.value, lastName.value] = newValue.split(' ');
-  },
-});
-
-function changeName(name) {
-  fullName.value = name;
+function changeActive() {
+  isActive.value = !isActive.value;
 }
+
+// 绑定数组
+const activeClass = ref('active');
+const fontStyleClass = ref('fontStyle');
+
+// 样式对象
+const styleObject = reactive({
+  color: 'red',
+  fontSize: '13px',
+});
 </script>
 
 <template>
-  <span>{{ fullName }} ,{{ firstName }} ,{{ lastName }} </span>
-  <button @click="changeName('Lea Bing')">改名</button>
+  <h1>绑定Class一</h1>
+  <div v-bind:class="{ active: isActive }">绑定样式</div>
+  <button @click="changeActive()">改变样式</button>
+  <h1>绑定Class二</h1>
+  <div
+    class="fontStyle"
+    v-bind:class="{ active: isActive, fontStyle2: isActive }"
+  >
+    绑定样式
+  </div>
+  <h1>绑定Class三</h1>
+  <div :class="classObject">绑定样式</div>
+
+  <h1>绑定Class三</h1>
+  <div :class="[activeClass, fontStyleClass]">绑定数组</div>
+
+  <h1>绑定Class四</h1>
+  <div :class="[isActive ? 'active' : 'fontStyle']">绑定数组</div>
+  <hr />
+
+  <h1>绑定Style</h1>
+  <div :style="{ 'font-size': 18 + 'px', color: 'blue' }">绑定Style</div>
+
+  <h1>绑定Style二</h1>
+  <div :style="styleObject">绑定Style</div>
 </template>
+
+<style>
+.fontStyle {
+  font-size: 45px;
+}
+
+.fontStyle2 {
+  font-size: 12px;
+}
+
+.active {
+  display: block;
+  color: red;
+}
+</style>
