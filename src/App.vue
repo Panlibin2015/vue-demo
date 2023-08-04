3e1c3b7694d43dfdda76ff6f4e312962cf188436
Panlibin2015 <18267568873@163.com>
2023-08-04 10:41:57 +0800

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,31 @@
 <script setup>
 import { ref, computed, reactive } from 'vue';
-const count = ref(0);
-const name = ref('Vue.js');
 
-function greet(event) {
-  alert(`Hello ${name.value}!`);
-  // `event` 是 DOM 原生事件
-  if (event) {
-    alert(event.target.tagName);
-  }
-}
-
-function say(message) {
-  alert(message);
-}
-
-function warn(message, event) {
-  // 这里可以访问原生事件
-  if (event) {
-    event.preventDefault();
-  }
-  alert(message);
-}
+const text = ref('Hello');
+const checked = ref(true);
+const checkedNames = ref([]);
 </script>
 
 <template>
-  <button @click="count++">按钮{{ count }}</button>
-  <button @click="greet()">Greet</button>
-  <button @click="say('Hello')">Say</button>
-  <button @click="warn('Say', $event)">Warn</button>
-  <!-- 使用内联箭头函数 -->
-  <button @click="(event) => warn('Form cannot be submitted yet.', event)">
-    Submit
-  </button>
+  <input :value="text" @input="(event) => (text = event.target.value)" />
+  <input v-model="text" />
+  <br />
+  <input v-model.lazy="text" />
+  <span>{{ text }}</span>
+  <hr />
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">{{ checked }}</label>
+  <hr />
+  <div>Checked names: {{ checkedNames }}</div>
+
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+  <label for="jack">Jack</label>
+
+  <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+  <label for="john">John</label>
+
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+  <label for="mike">Mike</label>
 </template>
 
 <style></style>
